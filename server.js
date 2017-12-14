e89e61c2ff87e047c9ab586ab4f1303871c536f3
Sahm Samarghandi <h4k4s4m@gmail.com>
2017-12-14 11:59:15 -0800

    changes

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 //-------------------------------------------------------------------------------------------------------
+
 var app = express();
 var PORT = process.env.PORT || 8080;
 //-------------------------------------------------------------------------------------------------------
+
 var db = require("./app/models");
 var iCSV = require('./import-csv.js');
+//-------------------------------------------------------------------------------------------------------
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -16,13 +19,15 @@ app.use(bodyParser.json({
     type: "application/vnd.api+json"
 }));
 //-------------------------------------------------------------------------------------------------------
+
 app.use(express.static("./app/assets"));
 //-------------------------------------------------------------------------------------------------------
-//-------------------------------------------------------------------------------------------------------
+
 // require("./app/routes/api-routes")(app);
 // require("./app/routes/html-routes")(app);
 require("./app/routes/sms-routes")(app);
 //-------------------------------------------------------------------------------------------------------
+
 db.sequelize.sync().then(function () {
     app.listen(PORT, function () {
         console.log("App listening on PORT " + PORT);
