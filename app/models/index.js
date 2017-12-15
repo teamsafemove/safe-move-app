dfd1c4b05bcd295de222f656a2d162b875b50ad9
Sahm Samarghandi <h4k4s4m@gmail.com>
2017-12-15 12:56:45 -0800

    cleardb testing

--- app/models/index.js
+++ app/models/index.js
@@ -7,6 +7,7 @@ var basename = path.basename(__filename);
 var env = process.env.NODE_ENV || 'heroku';
 var config = require(__dirname + '/../config/config.json')[env];
 var db = {};
+
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
