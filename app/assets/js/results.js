554f524824c3f3c84959873974e504374e4eb32c
Katrina Shainiak <ekaterina.shainiak@gmail.com>
2017-12-17 13:40:55 -0800

    created handlebar template(id="results-template") into results.html to render results for city search.

--- app/assets/js/results.js
+++ app/assets/js/results.js
@@ -2,8 +2,19 @@ $(document).ready(function () {
     $("#searchCity").submit(function (event) {
         event.preventDefault();
         var city = $('#city').val().trim();
-        $.get("/api/city/results", {city: city}, function (res) {
+        $.get("/api/city", {city: city}, function (res) {
+            var safeScore = res.city.violent_crimes + res.city.property_crimes;
+            res.city.Score = "fair";
+            if (safeScore < 20){
+                res.city.Score = "good";
+            };
+            if (safeScore > 30){
+                res.city.Score = "bad";
+            }
             console.log(res);
-        })
+            var theTemplateScript = $("#results-template").html();
+            var theTemplate = Handlebars.compile(theTemplateScript);
+            $("#results").html(theTemplate(res));
+        });
     });
 });
--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -3,15 +3,15 @@ var db = require("../models");
 
 module.exports = function (app) {
 
-    app.get("/api/city/results", function (req, res) {
+    app.get("/api/city", function (req, res) {
         searchCity = req.query.city;
         db.crimes.findOne({ where: { City: searchCity } }).then(function (city) {
             console.log("Found criminal records for:", req.query.city);
             db.zhvis.findAll({ where: { City: searchCity } }).then(function (zips) {
                 console.log("Found price records for:", req.query.city, zips.length);
-                // res.render("results", {city: city, zips: zips});            
+                res.json({city: city, zips: zips});
             })
-            res.json(city);
+            
         })
     });
 };
